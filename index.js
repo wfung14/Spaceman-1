650388e892d406fd81621883ab354f7b4aea7c88
wfung14 <wilson_fung1996@yahoo.com>
2023-01-05 20:03:01 -0500

    JS additions

---
+++ index.js
@@ -0,0 +1,4 @@
+const playButton = document.querySelector("#play-button")
+const hintButton = document.querySelector("#hint-button")
+const newGameButton = docment.querySelector("new-game-button")
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
